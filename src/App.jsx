8acd757682ee510906f6492c1684082c550322ee
Xinyi Wang <is.xinyiwang@outlook.com>
2021-12-01 16:52:43 +0100

    feat:add search bar functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import MovieGrid from './components/movieGrid/MovieGrid';
 import Movie from './components/movie/Movie';
+import Search from './components/search/Search';
 import data from './moviedata.json';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Search></Search>
         <MovieGrid>
           {data.map((movie, idx) => (
             <Movie movie={movie} key={idx} />
