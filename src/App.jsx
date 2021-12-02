c798fbcca3edc0db25bfb4c2aa6a7e48355f0a70
Hilda Robertsson <62420861+robertssonhilda@users.noreply.github.com>
2021-12-02 09:17:04 +0100

    feat: create display for selected movie (#5)
    * feat: create display for selected movie
    
    * fix: datatype for hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+import React, { useState } from 'react';
 import './App.scss';
 import MovieGrid from './components/movieGrid/MovieGrid';
 import Movie from './components/movie/Movie';
+import SelectedMovie from './components/selectedMovie/SelectedMovie';
 import data from './moviedata.json';
 
 const App = () => {
+  const [chosenMovie, setChosenMovie] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
-        <MovieGrid>
-          {data.map((movie, idx) => (
-            <Movie movie={movie} key={idx} />
-          ))}
-        </MovieGrid>
+        {chosenMovie ? (
+          <SelectedMovie chosenMovie={chosenMovie} setChosenMovie={setChosenMovie} />
+        ) : (
+          <MovieGrid>
+            {data.map((movie, idx) => (
+              <Movie movie={movie} key={idx} setChosenMovie={setChosenMovie} />
+            ))}
+          </MovieGrid>
+        )}
       </header>
     </div>
   );
